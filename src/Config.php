771a0d50ffdac15ec2f1ad9ec1d6417d5ca0f6c1
pawwys <carierchoice@student.edu>
2019-02-15 14:57:07 +0100

    Warsztaty

---
+++ src/Config.php
@@ -0,0 +1,6 @@
+<?php
+
+define('DATABASE_USER_NAME', 'pawwys');
+define('DATABASE_USER_PASSWORD', 'ubu');
+define('DATABASE_HOST', 'localhost');
+define('DATABASE_NAME', 'twitter');
---
+++ src/index.php
@@ -0,0 +1,19 @@
+<?php
+
+include('./header.php');
+
+$testUser = new User();
+$testUser->setEmail('test@o22.pl')->setHashPass('haslo')->setUserName('Nowak')->saveToDB($conn);
+
+var_dump($testUser);
+
+$testUser->setUserName('Kowalski')->setHashPass('noweHaslo')->setEmail('aaaa@onet.pl')->saveToDB($conn);
+
+var_dump($testUser);
+
+sleep(60);
+
+$testUser->delete($conn);
+
+include('./footer.php');
+
